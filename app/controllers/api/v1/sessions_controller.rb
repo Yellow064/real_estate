96a510e1d6c186754b7f0049e975f413ee230c5d
edoromanoo <edo.romanoo@gmail.com>
2015-05-13 00:59:31 -0500

    Adds destroy session action

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -15,7 +15,10 @@ class Api::V1::SessionsController < ApplicationController
 	end
 
 	def destroy
-		render json: { errors: "Invalid email or password" }, status: 204
+		user = User.find_by(auth_token: params[:id])
+		user.generate_authentication_token!
+		user.save
+		head 204
 	end
 
 	
