a556eb8e1a7fe35214626d977407825da4855184
edoromanoo <edo.romanoo@gmail.com>
2015-05-14 14:12:46 -0500

    Adds search scopes on the product model

--- app/models/house.rb
+++ app/models/house.rb
@@ -2,4 +2,17 @@ class House < ActiveRecord::Base
 	validates :title, :user_id, :date_published,  presence:true
 	validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
 	belongs_to :user
+	
+	scope :filter_by_title, lambda { |keyword|
+		where("lower(title) LIKE ?", "%#{keyword.downcase}%" ) 
+	}
+	scope :above_or_equal_to_price, lambda { |price| 
+		where("price >= ?", price) 
+	}
+	scope :below_or_equal_to_price, lambda { |price| 
+		where("price <= ?", price) 
+	}
+	scope :recent, -> {
+		order(:updated_at)
+	}
 end
--- spec/models/house_spec.rb
+++ spec/models/house_spec.rb
@@ -17,4 +17,69 @@ describe House do
 
   #validates link between the models
   it { should belong_to :user }
+
+
+  describe ".filter_by_title" do
+    before(:each) do
+      @house1 = FactoryGirl.create :house, title: "Cumbres"
+      @house2 = FactoryGirl.create :house, title: "Cumbres Elite"
+      @house3 = FactoryGirl.create :house, title: "Cumbres blah balh"
+      @house4 = FactoryGirl.create :house, title: "Garza Sada"
+
+    end
+
+    context "when a 'Cumbres' title pattern is sent" do
+      it "returns the 3 houses matching" do
+        expect(House.filter_by_title("Cumbres")).to have(3).items
+      end
+
+      it "returns the houses matching" do
+        expect(House.filter_by_title("Cumbres").sort).to match_array([@house1,@house2 ,@house3])
+      end
+    end
+  end
+
+
+  describe ".above_or_equal_to_price" do
+    before(:each) do
+      @house1 = FactoryGirl.create :house, price: 100
+      @house2 = FactoryGirl.create :house, price: 50
+      @house3 = FactoryGirl.create :house, price: 150
+      @house4 = FactoryGirl.create :house, price: 99
+    end
+
+    it "returns the houses which are above or equal to the price" do
+      expect(House.above_or_equal_to_price(100).sort).to match_array([@house1, @house3])
+    end
+  end
+
+  describe ".below_or_equal_to_price" do
+    before(:each) do
+      @house1 = FactoryGirl.create :house, price: 100
+      @house2 = FactoryGirl.create :house, price: 50
+      @house3 = FactoryGirl.create :house, price: 150
+      @house4 = FactoryGirl.create :house, price: 99
+    end
+
+    it "returns the houses which are above or equal to the price" do
+      expect(House.below_or_equal_to_price(99).sort).to match_array([@house2, @house4])
+    end
+  end
+
+  describe ".recent" do
+    before(:each) do
+      @house1 = FactoryGirl.create :house, price: 100
+      @house2 = FactoryGirl.create :house, price: 50
+      @house3 = FactoryGirl.create :house, price: 150
+      @house4 = FactoryGirl.create :house, price: 99
+
+      @house2.touch
+      @house3.touch
+    end
+
+    it "returns the most updated records" do
+      expect(House.recent).to match_array([@house3, @house2, @house4, @house1])
+    end
+  end
+
 end
